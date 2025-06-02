d333585187f51d6aa16b2fdb127876e7e961fcf3
AlanLR <alanlopes.r@gmail.com>
2025-06-02 13:33:59 -0300

    REFACT: criando uma propriedade para a string exchange

--- src/main/java/com/alanlr/proposta_app/service/PropostaService.java
+++ src/main/java/com/alanlr/proposta_app/service/PropostaService.java
@@ -6,24 +6,34 @@ import com.alanlr.proposta_app.entity.Proposta;
 import com.alanlr.proposta_app.mapper.PropostaMapper;
 import com.alanlr.proposta_app.repository.PropostaRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@AllArgsConstructor
 public class PropostaService {
 
     private PropostaRepository repository;
 
     private NotificacaoService notificacaoService;
 
+    private String exchange;
+
+    public PropostaService(PropostaRepository repository, NotificacaoService notificacaoService,
+                           @Value("${rabbitmq.propostapendente.exchange}")
+                           String exchange) {
+        this.repository = repository;
+        this.notificacaoService = notificacaoService;
+        this.exchange = exchange;
+    }
+
     public PropostaResponseDto criar(PropostaRequestDto data){
         Proposta proposta = PropostaMapper.INSTANCE.convertDtoToProposta(data);
         repository.save(proposta);
 
         PropostaResponseDto response = PropostaMapper.INSTANCE.convetEntityToDto(proposta);
-        notificacaoService.notificar(response, "proposta-pendente.ex");
+        notificacaoService.notificar(response, exchange);
 
         return response;
     }
